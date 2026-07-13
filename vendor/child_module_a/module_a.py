63cce5b2cd447d2d09f7efaf4ef04a5a5866a2ee
Copybara <copybara@example.com>
2026-07-13 21:12:07 +0000

    Squashed commits:   - 639cd048ef3ebab25e5c4346b3162b7e8e552f6f Add feature A3 by Copybara <copybara@example.com>   - 644648c30f47e9878136f57c6298b3817d1de319 Add feature A2 by Copybara <copybara@example.com>   - a8d7455829447d53bccae9ee3c7a12ab06dfc712 Add feature A1 by Copybara <copybara@example.com>   - 23ea06152ce65ead8e21a550b4ed8e3476790398 Update hello message to v2 by Copybara <copybara@example.com>
    GitOrigin-RevId: 639cd048ef3ebab25e5c4346b3162b7e8e552f6f

--- vendor/child_module_a/module_a.py
+++ vendor/child_module_a/module_a.py
@@ -1,2 +1,10 @@
 def hello_from_a():
-    return "Hello from Child Module A!"
+    return "Hello from Child Module A! (v2)"
+
+def feature_a1():
+    return "Feature A1"
+
+def feature_a2():
+    return "Feature A2"
+def feature_a3():
+    return "Feature A3"
